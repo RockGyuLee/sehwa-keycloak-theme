c39bd0836f1813ea3d23cd38151d703d5dc83e25
PoeySK <piz115@naver.com>
2026-02-23 16:21:19 +0900

    chore: 프로젝트 포매팅 및 eslint 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,57 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import pluginJs from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import airbnb from 'eslint-config-airbnb';
+import configPrettier from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+const baseConfig = {
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+      ...globals.node,
+      ...globals.mocha, // describe, it 인식
+      cy: 'readonly',
+      Cypress: 'readonly',
+    },
+    parser: tsParser,
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
     },
   },
-])
+  plugins: {
+    '@typescript-eslint': tseslint,
+    react: pluginReact,
+    prettier: pluginPrettier,
+  },
+  rules: {
+    ...pluginJs.configs.recommended.rules, // 기본 JS 규칙
+    ...tseslint.configs.recommended.rules, // TypeScript 규칙
+    ...pluginReact.configs.flat.recommended.rules, // React 규칙
+    ...airbnb.rules, // airbnb 규칙 사용
+    'prettier/prettier': ['error', { endOfLine: 'auto' }], // Prettier 규칙을 ESLint에서 에러로 처리
+    'react/react-in-jsx-scope': 'off', // React 17+에서 불필요한 규칙 비활성화
+  },
+};
+
+const prettierAndReactConfig = {
+  rules: {
+    ...configPrettier.rules, // Prettier와 충돌하는 규칙 비활성화
+    'react/jsx-props-no-spreading': 'off', // JSX에서 prop 전파 허용
+  },
+  ignores: ['node_modules', 'dist'], // 무시할 디렉토리
+};
+
+export default [
+  {
+    ...baseConfig,
+    files: ['**/*.{js,mjs,cjs,ts,tsx,jsx}'],
+  },
+  prettierAndReactConfig,
+];
